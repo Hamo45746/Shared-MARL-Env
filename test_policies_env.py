dd003ce099ec0a5e58930958128f5aca45d57151
AlexMW <alexmartinwallace@gmail.com>
2024-10-05 14:10:27 +1000

    test

--- test_policies_env.py
+++ test_policies_env.py
@@ -6,6 +6,6 @@ config_path = '/Users/alexandramartinwallace/Documents/Uni/METR4911/Working/Shar
 checkpoint_dir = '/Users/alexandramartinwallace/Documents/Uni/METR4911/Working/Shared-MARL-Env/custom_ray_results/'
 #checkpoint_dir = '/Users/alexandramartinwallace/Documents/Uni/METR4911/Working/Shared-MARL-Env/outputs/20:09 ray results/custom_ray_results/'
 params_path = '/Users/alexandramartinwallace/ray_results/PPO_custom_multi_agent_env_2024-10-04_16-48-23beq1d_at/params.pkl'
-    
+print('test')
 env2 = Environment(config_path)
 env2.run_simulation_with_policy(checkpoint_dir=checkpoint_dir, params_path=params_path, max_steps=900)
